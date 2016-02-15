51c28673598c6a440bd87ad68f52a50caa957606
Chris Von Hoene <cvonhoen@purdue.edu>
2016-02-14 20:36:37 -0500

    Lab 2 commit

--- pwcheck.sh
+++ pwcheck.sh
@@ -9,6 +9,10 @@ git commit -a -m "Lab 2 commit" >> .local.git.out
 SCORE=0
 pw=$(cat $1)
 
+if ${#pw} < 6 || ${#pw} > 32 ; then
+    echo "Error: Password length invalid."
+fi
+
 if grep -q [0-9] $1 ; then
     let SCORE=SCORE+1
 fi
