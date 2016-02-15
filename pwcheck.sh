7b795dd01c12608b26068a40ab02c6d0d8223686
Chris Von Hoene <cvonhoen@purdue.edu>
2016-02-14 20:24:14 -0500

    Lab 2 commit

--- pwcheck.sh
+++ pwcheck.sh
@@ -5,3 +5,9 @@ git add $0 >> .local.git.out
 git commit -a -m "Lab 2 commit" >> .local.git.out
 
 #Your code here
+SCORE=0
+if /usr/xpg4/bin/egrep -q [0-9] $1 ; then
+    let SCORE=SCORE+1
+if
+
+echo $SCORE
