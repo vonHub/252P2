93ff3cdc4619a2993e1029c8e5dc2de2c8b11e05
Chris Von Hoene <cvonhoen@purdue.edu>
2016-02-15 12:58:47 -0500

    Lab 2 commit

--- monitor.sh
+++ monitor.sh
@@ -129,7 +129,7 @@ function calculate_cpu_usage {
 function calculate_mem_usage
 {
 	#Let us extract the VmRSS value from /proc/{pid}/status
-
+    LINE=[ cat /proc/$PID/status | grep VmRSS ]
 
 	#Return the memory usage
 	echo "$mem_usage"
